a52283c1818072fd27e4f15428865a4521d79c2b
Benjamin <bmarquiscom@gmail.com>
2020-11-28 12:14:29 -0500

    0.0.18

--- __tests__/fixtures/helpers.ts
+++ __tests__/fixtures/helpers.ts
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/indent */
+import { APIGatewayProxyHandlerV2, APIGatewayProxyHandler } from "aws-lambda";
 import { LambdaFlowCallback } from "../../src/types";
 import { lambdaContext, lambdaEvent } from "./data";
 
-export const lambdaExecutor = async (lambdaFlowCallback: LambdaFlowCallback) =>
-  lambdaFlowCallback(lambdaEvent, lambdaContext, () => {});
+export const lambdaExecutor = async (
+  lambdaFlowCallback: LambdaFlowCallback<
+    APIGatewayProxyHandlerV2 | APIGatewayProxyHandler
+  >
+) => lambdaFlowCallback(lambdaEvent, lambdaContext, () => {});
